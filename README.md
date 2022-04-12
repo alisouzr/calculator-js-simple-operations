70a762a0edc80d0aa005284d1b10e71aeeef5a8a
Aliny <aliny.souz.r@gmail.com>
2022-04-12 17:09:53 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# calculadoraProjetoIniciante
+
+![image](https://user-images.githubusercontent.com/79667413/153730608-0df83eff-69db-46ab-bdf9-5f5f62b62942.png)
+
+Calculadora feita do zero, um projeto de iniciante que me ajudou a aplicar alguns conceitos que aprendi com cursos.
---
+++ main.js
@@ -0,0 +1,77 @@
+var valor;
+var displayTest;
+var displayTest2;
+
+function botao(num) {
+    valor = document.getElementById("display").value += num;
+
+    return valor;
+}
+
+function resetar() {
+    document.getElementById("display").value = '';
+}
+
+function subtracao() {
+    document.getElementById("display").value += '-'
+}
+
+function soma() {
+    document.getElementById("display").value += '+'
+}
+
+function divisao() {
+    document.getElementById("display").value += '/'
+}
+
+function multiplicar() {
+    document.getElementById("display").value += '*'
+}
+
+function ponto() {
+    document.getElementById("display").value += '.'
+}
+
+
+function igual() {
+    document.getElementById("display").value = ''
+    var inicial = 0;
+    var other = 1;
+    if (valor.includes("+")) {
+        var resultado = valor.split("+")
+        /* console.log(resultado.length) */
+        for (var i = 0; i < resultado.length; i++) {
+            var number = parseFloat(resultado[i])
+            inicial += number
+        }
+        document.getElementById("display").value += inicial
+    } else if (valor.includes("-")) {
+        var resultado = valor.split("-")
+        for (var i = 0; i < resultado.length; i++) {
+            var number = parseFloat(resultado[i])
+            if ((number.lengt - 1) > number[0]) {
+                inicial = inicial - number
+            } else {
+                inicial = - (inicial + number)
+            }
+
+        }
+        document.getElementById("display").value += inicial
+    } else if (valor.includes("*")) {
+        var resultado = valor.split("*")
+        /* console.log(resultado.length) */
+        for (var i = 0; i < resultado.length; i++) {
+            var number = parseFloat(resultado[i])
+            other *= number
+        }
+        document.getElementById("display").value += other
+    } else if (valor.includes("/")) {
+        var resultado = valor.split("/")
+        /* console.log(resultado.length) */
+        for (var i = 0; i < resultado.length; i++) {
+            var number = parseFloat(resultado[i])
+            other = (1 / number) / other
+        }
+        document.getElementById("display").value += other
+    }
+}
