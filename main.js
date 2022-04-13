c7d4d4ac1f1de9886822641f4de3ba1621368b42
Aliny <aliny.souz.r@gmail.com>
2022-04-13 16:41:26 -0300

    Update

--- main.js
+++ main.js
@@ -3,76 +3,81 @@ let valor;
 let displayTest;
 let displayTest2;
 //isso
-function botao(num) {
-    valor = document.getElementById("display").value += num;
+
+/* document.querySelector(".botao").addEventListener('click', (num) => {
+    let valor = document.querySelector("#display").value += num;
+
+    return valor;
+}) */
+
+const botao = (num) => {
+    valor = document.querySelector("#display").value += num;
 
     return valor;
 }
 
-function resetar() {
+document.querySelector('#clean-all').addEventListener('click', () => {
     document.getElementById("display").value = '';
-}
+})
 
-function subtracao() {
+document.querySelector('#sub').addEventListener('click', () => {
     document.getElementById("display").value += '-'
-}
+})
 
-function soma() {
+document.querySelector('#sum').addEventListener('click', () => {
     document.getElementById("display").value += '+'
-}
+})
 
-function divisao() {
+document.querySelector('#div').addEventListener('click', () => {
     document.getElementById("display").value += '/'
-}
+})
 
-function multiplicar() {
+document.querySelector('#mult').addEventListener('click', () => {
     document.getElementById("display").value += '*'
-}
+})
 
-function ponto() {
+document.querySelector('#dot').addEventListener('click', () => {
     document.getElementById("display").value += '.'
-}
+})
 
 
-function igual() {
+document.querySelector('#igual').addEventListener('click', () => {
     document.getElementById("display").value = ''
     var inicial = 0;
     var other = 1;
     if (valor.includes("+")) {
-        var resultado = valor.split("+")
+        var resultado = valor.split("+");
         /* console.log(resultado.length) */
         for (var i = 0; i < resultado.length; i++) {
-            var number = parseFloat(resultado[i])
-            inicial += number
+            var number = parseFloat(resultado[i]);
+            inicial += number;
         }
         document.getElementById("display").value += inicial
     } else if (valor.includes("-")) {
         var resultado = valor.split("-")
         for (var i = 0; i < resultado.length; i++) {
-            var number = parseFloat(resultado[i])
+            var number = parseFloat(resultado[i]);
             if ((number.lengt - 1) > number[0]) {
-                inicial = inicial - number
+                inicial = inicial - number;
             } else {
-                inicial = - (inicial + number)
+                inicial = - (inicial + number);
             }
 
         }
         document.getElementById("display").value += inicial
     } else if (valor.includes("*")) {
-        var resultado = valor.split("*")
-        /* console.log(resultado.length) */
+        var resultado = valor.split("*");
         for (var i = 0; i < resultado.length; i++) {
-            var number = parseFloat(resultado[i])
-            other *= number
+            var number = parseFloat(resultado[i]);
+            other *= number;
         }
         document.getElementById("display").value += other
     } else if (valor.includes("/")) {
-        var resultado = valor.split("/")
-        /* console.log(resultado.length) */
+        var resultado = valor.split("/");
         for (var i = 0; i < resultado.length; i++) {
-            var number = parseFloat(resultado[i])
-            other = (1 / number) / other
+            var number = parseFloat(resultado[i]);
+            other = (1 / number) / other;
         }
         document.getElementById("display").value += other
     }
-}
+});
